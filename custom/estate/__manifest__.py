7761ad0e55795fa7347506a604abab782d072e5c
Gonzalo Matamala <gonmator@gmail.com>
2023-01-22 14:26:06 +0100

    tutorial - capter 6 completado

--- custom/estate/__manifest__.py
+++ custom/estate/__manifest__.py
@@ -3,6 +3,8 @@
     'version': '0.0.1',
     'category': 'Sales',
     'depends': ['base'],
-    'data': ['security/ir.model.access.csv'],
+    'data': ['security/ir.model.access.csv',
+             'data/estate_property_views.xml',
+             'data/estate_menus.xml'],
     'application': True
 }
