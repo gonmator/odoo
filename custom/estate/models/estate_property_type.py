54882ed584eec7357a40be1a533773dae7aa0042
Gonzalo Matamala <gonmator@gmail.com>
2023-02-02 11:39:29 +0100

    tutorial - chapter 12 completado

--- custom/estate/models/estate_property_type.py
+++ custom/estate/models/estate_property_type.py
@@ -10,7 +10,7 @@ class EstatePropertyType(models.Model):
     name = fields.Char(required=True)
     property_ids = fields.One2many(comodel_name='estate.property', inverse_name='property_type_id')
     sequence = fields.Integer('Sequence', default=1)
-    offer_ids = fields.One2many(related='property_ids.offer_ids')
+    offer_ids = fields.One2many(comodel_name='estate.property.offer', inverse_name='property_type_id')
     offer_count = fields.Integer(compute='_compute_offer_count')
 
     @api.depends('offer_ids')
